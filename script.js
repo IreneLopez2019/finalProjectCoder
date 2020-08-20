a8bd6d8ee7145561af8ab2e70c1a2e388640a567
Irene <irene@miracledevs.com>
2020-08-20 12:04:49 -0300

    Scripts: Added modifiers to custom-cursor

--- script.js
+++ script.js
@@ -15,28 +15,46 @@ function setCursorListeners() {
     const span = document.querySelector(".custom-cursor span");
 
     document.addEventListener("pointermove", setCursorPosition);
-    document.addEventListener("pointerdown", ({ target }) => {
-        if (target.tagName !== "IMG") return;
-
-        cursor.classList.add("clicked");
-        span.innerText = target.alt;
+    document.addEventListener("pointerdown", (e) => {
+        setPointerContent(e.target.tagName);
+        // span.innerText = target.;
     });
 
     document.addEventListener("pointerup", () => {
-        cursor.classList.remove("clicked");
-        span.innerText = "";
+        cursor.classList = "custom-cursor";
+        // span.innerText = "";
     });
 }
 
 // Esta funcion crea el cursor y setea los listeners
 function createCursor() {
     cursor = document.createElement("div");
+    cursor = document.createElement("img");
     span = document.createElement("span");
     title = document.queryCommandValue("h1");
     cursor.append(span);
     cursor.classList.add("custom-cursor");
     document.body.append(cursor);
     setCursorListeners();
+}
+
+function setPointerContent(tag) {
+    const supportedTags = ["IMG", "P", "A"];
+
+    switch (tag) {
+        case (tag = supportedTags[0]):
+            cursor.classList.add("is-image");
+            break;
+        case (tag = supportedTags[1]):
+            cursor.classList.add("is-paragraph");
+            break;
+        case (tag = supportedTags[2]):
+            cursor.classList.add("is-item");
+            break;
+        default:
+            console.log("No es ninguna de las etiquetas soportadas");
+            break;
+    }
 }
 
 createCursor();
