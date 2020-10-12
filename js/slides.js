00932c46e85787d8bc71b341cdccc4104fc1fd5e
Irene Lopez <ire.lopez1998@gmail.com>
2020-10-12 11:35:32 -0300

    Added animations to footer and nav bar. removed a slide from slides.js

--- js/slides.js
+++ js/slides.js
@@ -3,30 +3,24 @@ export const slides = [
         id: "slide-1",
         title: "Doodles project",
         text: "",
-        background: "./assets/doodles-05.png"
+        background: "./assets/doodles-05.jpg"
     },
     {
         id: "slide-2",
         title: "Doodles project",
         text: "",
-        background: "../assets//doodles-03.png"
+        background: "../assets//doodles-03.jpg"
     },
     {
         id: "slide-3",
         title: "Doodles project",
         text: "",
-        background: "../assets//doodles-04.png"
+        background: "../assets//doodles-04.jpg"
     },
     {
         id: "slide-4",
         title: "Doodles project",
         text: "",
-        background: "../assets//doodles-02.png"
-    },
-    {
-        id: "slide-5",
-        title: "Doodles project",
-        text: "",
-        background: "../assets/img-5.jpg"
+        background: "../assets//doodles-02.jpg"
     }
 ]
